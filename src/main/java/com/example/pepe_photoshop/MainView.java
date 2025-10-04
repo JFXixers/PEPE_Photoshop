4414f7e361089185963b32e9bc6ea6d6a4174961
aRaptorix <198475180+aRaptorix@users.noreply.github.com>
2025-10-04 18:18:12 +0200

    Saving images
    You are able to save your loaded image.

--- src/main/java/com/example/pepe_photoshop/MainView.java
+++ src/main/java/com/example/pepe_photoshop/MainView.java
@@ -5,6 +5,12 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javafx.embed.swing.SwingFXUtils;
 
 public class MainView {
 
@@ -22,5 +28,26 @@ public class MainView {
         Image image = new Image(fileChooser.showOpenDialog(AnchorPane.getScene().getWindow()).toURI().toString());
 
         ImageView.setImage(image);
+    }
+
+    @FXML
+    public void saveImage() {
+        Image imageToSave = ImageView.getImage();
+
+        FileChooser fileChooserForSave = new FileChooser();
+        fileChooserForSave.getExtensionFilters().add(new FileChooser.ExtensionFilter("Images", "*.jpg", "*.jpeg", "*.bmp", "png"));
+        fileChooserForSave.setTitle("Save image");
+        File fileToSave = fileChooserForSave.showSaveDialog(AnchorPane.getScene().getWindow());
+        String nameOfFileToSafe = fileToSave.getName();
+
+        int indexOfDot = nameOfFileToSafe.lastIndexOf(".");
+        String format = nameOfFileToSafe.substring(indexOfDot+1);
+
+        BufferedImage buferedImg = SwingFXUtils.fromFXImage(imageToSave, null);
+        try {
+            ImageIO.write(buferedImg, format, fileToSave);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.pepe_photoshop {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
+    requires javafx.swing;
 
 
     opens com.example.pepe_photoshop to javafx.fxml;
